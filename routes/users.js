61e2071f086e4f85cf8b6329cad961c7fc669363
pscustomdev <pscustomdev@gmail.com>
2015-05-20 22:55:36 +0000

    we added main

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,18 @@ router.get('/create', function(req, res, next) {
     res.render('users/create');
 });
 
+router.post('/create', function(req, res, next) {
+    var somethingGoesWrong = false;
+    if (somethingGoesWrong) {
+        var vm = {
+            title: "Create an acccount",
+            input: req.body,
+            error: "Something went wrong"
+        };
+        delete vm.input.password;
+        return res.render("users/create", vm);
+    }
+    res.redirect('/main');
+});
+
 module.exports = router;

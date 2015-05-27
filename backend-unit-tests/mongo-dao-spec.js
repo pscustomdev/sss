bbfb6b140c23e6dc17db7b0ebcd25f479997667b
pscustomdev <pscustomdev@gmail.com>
2015-05-27 22:54:06 +0000

    implemented jasmine node karma

---
+++ backend-unit-tests/mongo-dao-spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var db = require('../db/mongo-dao');
+
+describe("Mongo Dao", function() {
+    it('should be able to call our fake function', function () {
+        expect(db.fakeFunction()).toEqual("blah");
+    });
+});

aee6c61a4dcc20ff11ab45ca62357e23be9391b9
pscustomdev <pscustomdev@gmail.com>
2015-08-06 23:15:31 +0000

    Added Overview, Details, factory for github api

---
+++ public/js/app/services/api-service.js
@@ -0,0 +1,57 @@
+(function() {
+    'use strict';
+    
+    angular
+        .module('app')
+        .factory('api', apiFactory);
+        
+    apiFactory.$inject = ['$http'];
+    
+    function apiFactory($http) {
+        return {
+            getSnippets : getSnippets,
+            getSnippetOverview : getSnippetOverview,
+            getSnippetDetail: getSnippetDetail
+            };
+        
+        function getSnippets() {
+            return $http.get('/api/snippets')
+                .then(function(response) {
+                    return response.data;
+                },
+                function(reason) {
+                    console.log(reason);
+                })
+                .catch(function(err) {
+                    console.log(err);    
+                });
+        }
+
+        function getSnippetOverview(snippetId) {
+            return $http.get('/api/snippet-overview/' + snippetId)
+                .then(function(response) {
+                    return response.data;
+                },
+                function(reason) {
+                    console.log(reason);
+                })
+                .catch(function(err) {
+                    console.log(err);    
+                });
+       }
+
+        function getSnippetDetail(snippetId, fileName) {
+            return $http.get('/api/snippet-detail/' + snippetId + "/" + fileName)
+                .then(function(response) {
+                    return response.data;
+                },
+                function(reason) {
+                    console.log(reason);
+                })
+                .catch(function(err) {
+                    console.log(err);    
+                });
+       }
+
+    }
+}());

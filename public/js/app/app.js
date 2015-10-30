94ceae935d3f0607dd5f37998d36d3b8a9ad523d
TNosredna <tnosredna@gmail.com>
2015-10-29 18:20:42 -0600

    Refactored app.js with new understanding as well as to keep it stylistically the same as the other views/controllers.

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -2,37 +2,36 @@
     'use strict';
 
     // Declare app level module which depends on views, and components
-    angular.module('app', [
-            'ui.router',
-            'app.angularService',
-            'app.searchService',
-            'app.search',
-            'app.results'
-        ])
-        .config(['$urlRouterProvider',
-            function($urlRouterProvider) {
-                $urlRouterProvider.otherwise('/search');    // Sets default view to render
-            }
-        ])
-        .directive('ngEnter', function() {
-            return function(scope, element, attrs) {
-                element.bind("keydown keypress", function(event) {
-                    if(event.which === 13) {
-                        scope.$apply(function(){
-                            scope.$eval(attrs.ngEnter, {'event': event});
-                        });
+    angular.module('app', ['ui.router', 'app.$searchService', 'app.search', 'app.results'])
+        .config(['$urlRouterProvider', URLRouteProvider])
+        .directive('ngEnter', ngEnter)
+        .run(main);
 
-                        event.preventDefault();
-                    }
-                });
-            };
-        })
-        .run(
-            function ($rootScope, $state, $stateParams, $log) {
-                $rootScope.$log = $log;
-                $rootScope.$state = $state;
-                $rootScope.$stateParams = $stateParams;
-            }
-        );
+    URLRouteProvider.$inject = ['$urlRouterProvider'];
+    main.$inject = ['$rootScope', '$searchService', '$state', '$stateParams', '$log'];
 
+    function URLRouteProvider($urlRouterProvider) {
+        $urlRouterProvider.otherwise('/search');    // Sets default view to render
+    }
+
+    function ngEnter() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.ngEnter, {'event': event});
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    }
+
+    function main($rootScope, $searchService, $state, $stateParams, $log) {
+        $rootScope.$log = $log;
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+        $rootScope.$searchService = $searchService;
+    }
 }());

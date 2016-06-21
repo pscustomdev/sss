500cc25a7485e3880fd56ec0bdf033abadb4c166
Kent <register@gividen.com>
2016-06-21 17:15:52 -0600

    made db file path relative.

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-var mongoFilePath = (process.env.NODE_ENV === 'production') ? 'd:/home/site/db/mongoDBFiles' : '/Users/kgividen/Data/Novell/sss/mongoDBFiles';
-
 module.exports = {
     serverPort: 3000,
     //mongoUri: 'mongodb://localhost:27017/sss',
-    mongoFilePath: mongoFilePath,
+    mongoFilePath: 'mongoDBFiles',
     cookieMaxAge: 30 * 24 * 3600 * 1000
 };

63b829927dd9fef07c6028995b6c4309cc5b7f80
Kent <register@gividen.com>
2016-06-21 17:18:40 -0600

    Change relative path based on prod in azure or not.

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+var mongoFilePath = (process.env.NODE_ENV === 'production') ? '../mongoDBFiles' : 'mongoDBFiles';
+
 module.exports = {
     serverPort: 3000,
     //mongoUri: 'mongodb://localhost:27017/sss',
-    mongoFilePath: 'mongoDBFiles',
+    mongoFilePath: mongoFilePath,
     cookieMaxAge: 30 * 24 * 3600 * 1000
 };

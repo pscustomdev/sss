123033d8fb4043170cbfd7346ecd5a2462a370c8
Robert Anderson <robertanderson@randerson.dnsdhcp.provo.novell.com>
2016-06-23 11:07:36 -0600

    updated url to remove port

--- auth/auth-conf.js
+++ auth/auth-conf.js
@@ -4,21 +4,21 @@ var cfg = require('../config.js');
 var githubApiToken = process.env.GithubApiToken;
 var githubClientID = process.env.GithubClientID;
 var githubClientSecret = process.env.GithubClientSecret;
-var githubCallbackURL = "http://www.softwaresnippetsearch.com";
+var githubCallbackURL = "http://www.softwaresnippetsearch.com/auth/github/callback";   // If this url ever changes in ANY way (eg http -> https), sss-storage's configured application must be updated
 
 if (process.env.NODE_ENV !== 'production') {
     var authConfLocal = require('../auth/auth-conf-local.js');
     githubApiToken =  authConfLocal.github_api.token;
     githubClientID = authConfLocal.github.clientID;
     githubClientSecret =  authConfLocal.github.clientSecret;
-    githubCallbackURL = "http://localhost";
+    githubCallbackURL = "http://localhost:" + cfg.serverPort + '/auth/github/callback';
 }
 
 module.exports = {
     github: {
         clientID: githubClientID,
         clientSecret: githubClientSecret,
-        callbackURL: githubCallbackURL + ":" + cfg.serverPort + '/auth/github/callback'
+        callbackURL: githubCallbackURL
     },
     github_api: {
         username: 'pscustomdev-sss',

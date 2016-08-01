01115f5d7ba32eb646511f6b0b9074b3b0a9f9e8
Kent <register@gividen.com>
2016-08-01 13:37:00 -0600

    Fixed name

--- gruntfile.js
+++ gruntfile.js
@@ -238,7 +238,7 @@ module.exports = function(grunt) {
 
     grunt.registerTask('backend-tests', ['mochaTest:single-pass']);
     grunt.registerTask('default', ['sss-development-mode']);
-    grunt.registerTask('deploy-production', ['clean', 'concat', 'uglify', 'remove']);
+    grunt.registerTask('build-production', ['clean', 'concat', 'uglify', 'remove']);
     //grunt.registerTask('build-production', ['run-all-tests','clean', 'concat', 'uglify']);
     grunt.registerTask('end2end-tests', ['protractor:single-pass']);
     grunt.registerTask('frontend-tests', ['karma:single-pass']);

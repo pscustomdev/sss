3034009d30cfcb3bcc75c4480a70ea9d8f8fd732
Kent <register@gividen.com>
2016-08-02 17:29:24 -0600

    Fixed logic for environment

--- auth/auth-conf.js
+++ auth/auth-conf.js
@@ -1,12 +1,17 @@
 var cfg = require('../config.js');
 
 //This is so we can set environment variables for the tokens in prod and not have them checked into git
-var githubApiToken = process.env.GithubApiToken;
-var githubClientID = process.env.GithubClientID;
-var githubClientSecret = process.env.GithubClientSecret;
-var githubCallbackURL = "http://www.softwaresnippetsearch.com/auth/github/callback";   // If this url ever changes in ANY way (eg http -> https), sss-storage's configured application must be updated
+var githubApiToken =  "";
+var githubClientID = "";
+var githubClientSecret =  "";
+var githubCallbackURL = "";
 
-if (process.env.NODE_ENV !== 'production' || process.env.NODE_ENV !== 'testing') {
+if (process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'testing') {
+    githubApiToken = process.env.GithubApiToken;
+    githubClientID = process.env.GithubClientID;
+    githubClientSecret = process.env.GithubClientSecret;
+    githubCallbackURL = "http://www.softwaresnippetsearch.com/auth/github/callback";   // If this url ever changes in ANY way (eg http -> https), sss-storage's configured application must be updated
+} else {
     var authConfLocal = require('../auth/auth-conf-local.js');
     githubApiToken =  authConfLocal.github_api.token;
     githubClientID = authConfLocal.github.clientID;
